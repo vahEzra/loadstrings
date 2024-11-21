b8e5ff661b3939c214b2f020e7caf78d00e69d19
vahEzra <lifecycle702@gmail.com>
2024-11-21 12:04:25 -0500

    Create mainSource.lua

---
+++ darkLands/mainSource.lua
@@ -0,0 +1,5 @@
+local key = _G.accessKey
+
+if key == "18-sysopclan-Chinhooks_DT-18" then
+  print("correct key, adding this Ui soon. (REMOTES: game.ReplicatedStorage.Remotes.Revive:FireServer()")
+end
